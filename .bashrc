91f882bfb968d86ec2ae7714cc890fe725e3503a
Ghiles <ghiles.larbi@fgei.ummto.dz>
2023-10-01 08:11:19 +0100

    move nvim to another repo

--- .bashrc
+++ .bashrc
@@ -1,4 +1,3 @@
-
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
--- .config/nvim/lua/core/plugins_config/init.lua
+++
@@ -1,7 +0,0 @@
-pcall( require, "core.plugins_config.nvim-tree")
-pcall( require, "core.plugins_config.lualine")
-pcall( require, "core.plugins_config.treesitter")
-pcall( require, "core.plugins_config.completions")
-pcall( require, "core.plugins_config.lsp_config")
-pcall( require, "core.plugins_config.barbar")
-pcall( require, "core.plugins_config.colorizer")
